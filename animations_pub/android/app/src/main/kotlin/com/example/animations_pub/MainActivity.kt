df17d28819d19c239ac65f67a90b533a712b9766
Alejo <carlos.velam@uniagustiniana.edu.co>
2020-09-09 09:53:31 -0500

    initial Commit

---
+++ animations_pub/android/app/src/main/kotlin/com/example/animations_pub/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animations_pub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ animations_pub/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'src/homePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
